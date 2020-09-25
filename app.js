7b7506892356074d4113afd6c9ee5b6524ed0eed
JustinMartin7x <justin.martin7x@gmail.com>
2020-09-25 16:33:30 -0700

    added some java and html

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 // initialize state
 
 // set event listeners to update state and DOM
+const input1 = document.getElementById ('input-1');
+const input2 = document.getElementById ('input-2');
+const input3 = document.getElementById ('input-3');
+const input4 = document.getElementById ('input-4');
+const input5 = document.getElementById ('input-5');
+const input6 = document.getElementById ('input-6');
+const input7 = document.getElementById ('input-7');
+const input8 = document.getElementById ('input-8');
+const input9 = document.getElementById ('input-9');
+const button =
+
+
